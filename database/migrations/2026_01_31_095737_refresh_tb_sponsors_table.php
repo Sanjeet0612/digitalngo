60b178da14a7a951ba2ce33b7f04e3ca42b89daa
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-01-31 15:29:51 +0530

    Create 2026_01_31_095737_refresh_tb_sponsors_table.php

---
+++ database/migrations/2026_01_31_095737_refresh_tb_sponsors_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::dropIfExists('tb_sponsors'); // drop existing
+        Schema::create('tb_sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone', 20);
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->string('state', 100)->nullable();
+            $table->string('city', 100)->nullable();
+            $table->string('zipcode', 60)->nullable();
+            $table->text('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('website')->nullable();
+            $table->enum('sponsor_type', ['gold','silver','bronze'])->default('bronze');
+            $table->timestamps();
+            $table->integer('status')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_sponsors');
+    }
+};
