92a90d4f7bccf9fa6fc92826256a468715d9d3b7
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-01-31 10:56:11 +0530

    Create Contact.php

---
+++ app/Models/Admin/Contact.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_contact';
+
+    protected $fillable = [
+        'phone',
+        'emailid',
+        'workingDays',
+        'officeTime',
+        'address',
+        'city',
+        'state',
+        'zipcode',
+        'country',
+        'fb_link',
+        'twitter_link',
+        'insta_link',
+        'behance_link',
+        'youtube_link',
+        'copyright',
+        'created_by',
+        'created_by_url',
+        'status',
+    ];
+}
