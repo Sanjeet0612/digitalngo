f353bfe8777963b69f427a774bfe82490907c58f
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-01-31 09:17:35 +0530

    Update volunteers.blade.php

--- resources/views/front/volunteers.blade.php
+++ resources/views/front/volunteers.blade.php
@@ -1,3 +1,258 @@
 @include('front.includes.new_header')
 
+            <!-- Page Breadcrumbs Start -->
+    <section class="breadcrumbs-page-wrap">
+        <div class="bg-fixed pos-rel breadcrumbs-page">
+            <div class="container">
+                <h1>Our Volunteers Teams</h1>
+                <nav aria-label="breadcrumb" class="breadcrumb-wrap">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Our Volunteers</li>
+                    </ol>
+                </nav>  
+            </div>
+        </div>
+    </section>
+    <!-- Page Breadcrumbs End -->
+
+    <!-- Main Body Content Start -->
+    <main id="body-content">
+
+        <!-- About Us Style Start -->
+        <section class="wide-tb-100">
+            <div class="container">
+                <h1 class="heading-main">
+                    <small>Team Member</small>
+                    Volunteers Teams
+                </h1>
+                <div class="row">
+                    
+                   
+
+
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img green">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-5.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img green">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-5.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img green">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-5.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img green">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-5.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img green">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-5.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img navy-blue">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-5.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img orange">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-7.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+
+                    <!-- Team Column One -->
+                    <div class="col-12 col-lg-3 col-sm-6">
+                        <div class="team-section-wrap">
+                            <div class="img beige">
+                                <div class="social-icons">
+                                    <a href="#"><i class="icofont-facebook"></i></a>
+                                    <a href="#"><i class="icofont-twitter"></i></a>
+                                    <a href="#"><i class="icofont-instagram"></i></a>
+                                </div>
+                                <img src="assets/images/team/team-8.jpg" alt="" class="rounded-circle">
+                            </div>
+                            <h4>Your Name</h4>
+                            <h5>Volunteers </h5>
+                            <div class="text-md-right">
+                                <a href="javascript:" class="read-more-line"><span>Read More</span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                </div>
+            </div>
+        </section>
+        <!-- About Us Style Start -->
+
+      
+        <!-- Team Member Style Start -->
+        <section class="wide-tb-100 mb-spacer-md pb-0">
+            <div class="container">
+            </div>
+        </section>
+        <!-- Team Member Style End -->
+
+        <!-- Faq's Style Start -->
+        <section class="wide-tb-100 pattern-orange pt-0 mb-spacer-md">
+            <div class="container">
+                <div class="row d-flex align-items-center" style="padding-top: 80px;">
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">
+                            <div class="counter-txt"><span class="counter">180</span>+</div>
+                            <div>Featured Campaign</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">               
+                            <div class="counter-txt"><span class="counter">7120</span>+</div>
+                            <div>Money Raised</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+
+                    <!-- Spacer For Medium -->
+                    <div class="w-100 d-none d-sm-block d-lg-none spacer-60"></div>
+                    <!-- Spacer For Medium -->
+
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">
+                            <div class="counter-txt"><span class="counter">250</span>+</div>
+                            <div>Dedicated Volunteers</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">     
+                            <div class="counter-txt"><span class="counter">1530</span>+</div>
+                            <div>People Helped Happily</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+                </div>
+            </div>
+        </section>
+        <!-- Faq's Style End -->
+
+        <!-- Our Partners Start -->
+        @include('front.includes.new_partner')
+        <!-- Our Partners End -->
+           
+    </main>
+
+  <!-- Main Footer Start -->
+
 @include('front.includes.new_footer')
