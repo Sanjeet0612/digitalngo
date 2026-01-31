523472d6e98e288db8dca0a4cdd80fe775daf49d
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-01-31 14:12:48 +0530

    Create Sponsor.php

---
+++ app/Models/Admin/Sponsor.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsor extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_sponsors';
+
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+        'address',
+        'state',
+        'city',
+        'zipcode',
+        'description',
+        'logo',
+        'website',
+        'banner',
+        'sponsor_type',
+        'status',
+    ];
+}
