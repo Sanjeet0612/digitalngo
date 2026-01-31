79a664154c1814c6aaea1582da480047d34fd2cd
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-01-31 09:26:52 +0530

    Update video.blade.php

--- resources/views/front/video.blade.php
+++ resources/views/front/video.blade.php
@@ -1,3 +1,102 @@
 @include('front.includes.new_header')
 
+          <!-- Page Breadcrumbs Start -->
+    <section class="breadcrumbs-page-wrap">
+        <div class="bg-fixed pos-rel breadcrumbs-page">
+            <div class="container">
+                <h1>Our Video</h1>
+                <nav aria-label="breadcrumb" class="breadcrumb-wrap">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Our Video</li>
+                    </ol>
+                </nav>  
+            </div>
+        </div>
+    </section>
+    <!-- Page Breadcrumbs End -->
+
+    <!-- Main Body Content Start -->
+    <main id="body-content">
+
+        <!-- About Us Style Start -->
+        <section class="wide-tb-100">
+            <div class="container">
+                
+                <div class="row">
+                     <!-- Team Column One -->
+                    <div class="col-12 col-lg-6 col-sm-6">
+                        <div class="team-section-wrap">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/D6kfjvqMJnI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                     <!-- Team Column One -->
+                    <div class="col-12 col-lg-6 col-sm-6">
+                        <div class="team-section-wrap">
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/3TKI1d7K5IA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                    <!-- Team Column One -->
+                </div>
+            </div>
+        </section>
+        <!-- About Us Style Start -->   
+        <!-- Faq's Style Start -->
+        <section class="wide-tb-100 pattern-orange pt-0 mb-spacer-md">
+            <div class="container">
+                <div class="row d-flex align-items-center" style="padding-top: 80px;">
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">
+                            <div class="counter-txt"><span class="counter">180</span>+</div>
+                            <div>Featured Campaign</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">               
+                            <div class="counter-txt"><span class="counter">7120</span>+</div>
+                            <div>Money Raised</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+
+                    <!-- Spacer For Medium -->
+                    <div class="w-100 d-none d-sm-block d-lg-none spacer-60"></div>
+                    <!-- Spacer For Medium -->
+
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">
+                            <div class="counter-txt"><span class="counter">250</span>+</div>
+                            <div>Dedicated Volunteers</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+
+                    <!-- Counter Col Start -->
+                    <div class="col col-12 col-lg-3 col-sm-6">
+                        <div class="counter-style-box">     
+                            <div class="counter-txt"><span class="counter">1530</span>+</div>
+                            <div>People Helped Happily</div>
+                        </div>
+                    </div>
+                    <!-- Counter Col End -->
+                </div>
+            </div>
+        </section>
+        <!-- Faq's Style End -->
+
+        <!-- Our Partners Start -->
+         @include('front.includes.new_partner')
+        <!-- Our Partners End -->
+        
+           
+    </main>
+
+  <!-- Main Footer Start -->
+
 @include('front.includes.new_footer')
