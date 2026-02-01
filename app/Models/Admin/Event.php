c6add8f67784985356d7e2d19b462d489a1abc51
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-01 14:51:43 +0530

    Create Event.php

---
+++ app/Models/Admin/Event.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $table = 'tb_event';
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'og_name',
+        'og_email',
+        'og_phone',
+        'og_weblink',
+        'description',
+        'start_date',
+        'end_date',
+        'location',
+        'user_id',
+        'banner',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date'   => 'date',
+    ];
+}
