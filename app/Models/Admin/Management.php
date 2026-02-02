cf3ed10ec6cf17b210394b8e82185d0dc25ab699
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-02 16:32:31 +0530

    Create Management.php

---
+++ app/Models/Admin/Management.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Management extends Model
+{
+    use HasFactory;
+
+    // agar table ka naam tb_team hai
+    protected $table = 'tb_management';
+
+    protected $fillable = [
+        'm_name',
+        'emailid',
+        'phone',
+        'address',
+        'city',
+        'state',
+        'zip_code',
+        'fb_link',
+        'twt_link',
+        'insta_link',
+        'description',
+        'profile_img',
+        'status'
+    ];
+}
+
