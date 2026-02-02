dc6e6e55e8cde234f8e0d6c71cbb854501c532a9
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-02 15:36:02 +0530

    Update TeamController.php

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 class TeamController extends Controller
 {
     public function management_team(Request $request){
-        echo "management_team";
+        return view('admin.team.management_team');
     }
     public function volunteers_team(Request $request){
-        echo "volunteers_team";
+        return view('admin.team.volunteers_team');
     }
 }
