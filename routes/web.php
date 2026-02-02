f9028a8c24ffd6851597f1a43ba7071c9bb93d20
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-02 14:36:20 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -136,7 +136,7 @@ Route::prefix('admin')->group(function () {
         Route::get('/manage-event-gallery', [EventGalleryController::class, 'manage_event_gallery'])->name('admin.manage_event_gallery');
         Route::match(['get', 'post'], '/add-event-gallery', [EventGalleryController::class, 'add_event_gallery'])->name('admin.add-event-gallery');
         Route::get('/edit-event-gallery/{id}', [EventGalleryController::class, 'edit_event_gallery'])->name('admin.edit-event-gallery');
-        Route::get('/update-event-gallery/{id}', [EventGalleryController::class, 'update_event_gallery'])->name('admin.update-event-gallery');
+        Route::put('/update-event-gallery/{id}', [EventGalleryController::class, 'update_event_gallery'])->name('admin.update-event-gallery');
 
 
         
