a48fe6743deb1b5a13b4efd7a1925e859dc116e7
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-03 14:07:55 +0530

    Update GalleryController.php

--- app/Http/Controllers/Admin/GalleryController.php
+++ app/Http/Controllers/Admin/GalleryController.php
@@ -8,7 +8,15 @@ use Illuminate\Http\Request;
 class GalleryController extends Controller
 {
     public function picture_category(Request $request){
-        return view('admin.gallery.manage_category');
+        $banners = array(); 
+        return view('admin.gallery.manage_picture_category',compact('banners'));
+    }
+    public function add_category(Request $request){
+        if($request->isMethod('post')){
+
+        }else{
+            return view('admin.gallery.add_category_form');
+        }
     }
     public function gallery_picture(Request $request){
 
