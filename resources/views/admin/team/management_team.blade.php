78210a94f724626181844dcec0c89d5ac7aca149
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-03 12:25:34 +0530

    Update management_team.blade.php

--- resources/views/admin/team/management_team.blade.php
+++ resources/views/admin/team/management_team.blade.php
@@ -72,8 +72,8 @@
                                 </div>
 
                                 <div class="ps-16 pb-16 pe-16" style="margin-top:8%">
-                                    <h6 class="text-lg mb-0 mt-4">{{$eventListVal->m_name}} @if($eventListVal->status==1)<span class="text-success" style="font-size:13px;">Active</span> @else <span class="text-danger" style="font-size:13px;">Dective</span> @endif</h6>
-                                    <p class="text-line-3 text-neutral-500">{{strip_tags($eventListVal->description)}}</p>
+                                    <h6 class="text-lg mb-0 mt-4">{{$eventListVal->m_name}} - {{$eventListVal->designation}} @if($eventListVal->status==1)<span class="text-success" style="font-size:13px;">Active</span> @else <span class="text-danger" style="font-size:13px;">Dective</span> @endif</h6>
+                                    <p class="text-line-3 text-neutral-500">{{$eventListVal->team_type}}</p>
 
                                     <div>
                                             <span class=" text-md text-secondary-light text-sm mb-0"><i class="ri-mail-line"></i> {{$eventListVal->emailid}}</span>
