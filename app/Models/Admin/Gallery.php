18e4cd230ab39a763b20eef25db5a37677f34241
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-04 10:18:14 +0530

    Update Gallery.php

--- app/Models/Admin/Gallery.php
+++ app/Models/Admin/Gallery.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Gallery extends Model
 {
     use HasFactory;
+
+    protected $table = 'tb_gallery';
+
+    protected $fillable = [
+        'cat_id',
+        'gtype',
+        'path',
+        'status',
+    ];
+
+    /**
+     * Gallery belongs to Category
+     */
+    public function category()
+    {
+        return $this->belongsTo(GalleryCategory::class, 'cat_id');
+    }
 }
