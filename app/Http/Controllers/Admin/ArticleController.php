b8245e7bfebb95774d02701ea6f2824535fd8edb
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-04 16:02:30 +0530

    Update ArticleController.php

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller{
     
     public function manage_articles(Request $request){
-        return view('');
+        $title = "Blog";
+        $blogs = array();
+        return view('admin.articles.manage_article',compact('title','blogs'));
+    }
+
+    public function add_articles(Request $request){
+        $blogs = array();
+        return view('admin.articles.add_articles_form',compact('blogs'));
     }
 }
