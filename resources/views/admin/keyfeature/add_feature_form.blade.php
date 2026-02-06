2f56b4185c094ee80be4df9d78cb1b4ab37491f5
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-06 12:54:37 +0530

    Update add_feature_form.blade.php

--- resources/views/admin/keyfeature/add_feature_form.blade.php
+++ resources/views/admin/keyfeature/add_feature_form.blade.php
@@ -44,6 +44,29 @@
             <div class="row gy-4">
                 <div class="col-lg-12">
                     <div class="card mt-24">
+
+
+                        <div class="messages">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @if(session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                     @if(session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+                </div>
                         <div class="card-header border-bottom">
                             <h6 class="text-xl mb-0">Add New Features</h6>
                         </div>
