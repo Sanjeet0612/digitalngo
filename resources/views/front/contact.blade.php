1c32c5343e9c54a1cee9ebd6be61546f0de6e86d
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-06 10:58:33 +0530

    Update contact.blade.php

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -7,7 +7,7 @@
                 <h1>Contact Us</h1>
                 <nav aria-label="breadcrumb" class="breadcrumb-wrap">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                        <li class="breadcrumb-item"><a href="{{url('/')}}">Home</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Contact Us</li>
                     </ol>
                 </nav>  
