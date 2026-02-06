71aff2e2ed7342a17d6219ca294aa4c9609459fa
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-06 12:18:11 +0530

    Update KeyFeatureController.php

--- app/Http/Controllers/Admin/KeyFeatureController.php
+++ app/Http/Controllers/Admin/KeyFeatureController.php
@@ -13,7 +13,12 @@ class KeyFeatureController extends Controller{
     }
     public function add_key_feature(Request $request){
         if($request->isMethod('post')){
-
+            $request->validate([
+                'title'  => 'required',
+                'phone'  => 'required',
+                'status' => 'required',
+                'iconimage.*' => 'image|mimes:jpeg,png,jpg|max:500', // validate each file
+            ]);
         }else{
            
             return view('admin.keyfeature.add_feature_form');
