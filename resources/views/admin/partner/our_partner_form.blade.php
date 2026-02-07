1f7fc5d5c0c8f65de700b2caba28747ac317e1a2
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-07 09:55:24 +0530

    Update our_partner_form.blade.php

--- resources/views/admin/partner/our_partner_form.blade.php
+++ resources/views/admin/partner/our_partner_form.blade.php
@@ -44,29 +44,27 @@
             <div class="row gy-4">
                 <div class="col-lg-12">
                     <div class="card mt-24">
-
-
                         <div class="messages">
-                    @if($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
+                            @if($errors->any())
+                                <div class="alert alert-danger">
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
+                                </div>
+                            @endif
+                            @if(session('success'))
+                                <div class="alert alert-success">
+                                    {{ session('success') }}
+                                </div>
+                            @endif
+                            @if(session('error'))
+                                <div class="alert alert-danger">
+                                    {{ session('error') }}
+                                </div>
+                            @endif
                         </div>
-                    @endif
-                    @if(session('success'))
-                        <div class="alert alert-success">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                     @if(session('error'))
-                        <div class="alert alert-danger">
-                            {{ session('error') }}
-                        </div>
-                    @endif
-                </div>
                         <div class="card-header border-bottom">
                             <h6 class="text-xl mb-0">Add New Partners</h6>
                         </div>
