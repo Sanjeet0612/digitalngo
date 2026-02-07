de44fdc77b22b5ceec1548a0b40d3ae2f8acface
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-07 09:55:29 +0530

    Create edit_partner_form.blade.php

---
+++ resources/views/admin/partner/edit_partner_form.blade.php
@@ -0,0 +1,106 @@
+@extends('admin.layout.layout')
+@php
+    $title='Edit & Update Partners';
+    $subTitle = 'Edit & Update Partners';
+    $script = '<script src="' . asset('assets/js/editor.highlighted.min.js') . '"></script>
+                 <script src="' . asset('assets/js/editor.quill.js') . '"></script>
+                 <script src="' . asset('assets/js/editor.katex.min.js') . '"></script>
+                 <script>
+                 // Editor Js Start
+                 const quill = new Quill("#editor", {
+                     modules: {
+                         syntax: true,
+                         toolbar: "#toolbar-container",
+                     },
+                     placeholder: "Compose an epic...",
+                     theme: "snow",
+                 });
+                 // Editor Js End
+             
+             
+                 // =============================== Upload Single Image js start here ================================================
+                 const fileInput = document.getElementById("upload-file");
+                 const imagePreview = document.getElementById("uploaded-img__preview");
+                 const uploadedImgContainer = document.querySelector(".uploaded-img");
+                 const removeButton = document.querySelector(".uploaded-img__remove");
+             
+                 fileInput.addEventListener("change", (e) => {
+                     if (e.target.files.length) {
+                         const src = URL.createObjectURL(e.target.files[0]);
+                         imagePreview.src = src;
+                         uploadedImgContainer.classList.remove("d-none");
+                     }
+                 });
+                 removeButton.addEventListener("click", () => {
+                     imagePreview.src = "";
+                     uploadedImgContainer.classList.add("d-none");
+                     fileInput.value = "";
+                 });
+                 // =============================== Upload Single Image js End here ================================================
+                 </script>';
+@endphp
+
+@section('content')
+            <div class="row gy-4">
+                <div class="col-lg-12">
+                    <div class="card mt-24">
+
+
+                        <div class="messages">
+                    @if($errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+                    @if(session('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                    @endif
+                     @if(session('error'))
+                        <div class="alert alert-danger">
+                            {{ session('error') }}
+                        </div>
+                    @endif
+                </div>
+                        <div class="card-header border-bottom">
+                            <h6 class="text-xl mb-0">Add New Partners</h6>
+                        </div>
+                        <div class="card-body p-24">
+                            <form id="myForm" action="{{route('admin.update_partners',$partnerData->id)}}" method="post" class="d-flex flex-column gap-20" enctype="multipart/form-data">
+                                @csrf
+                                @method('PUT')
+                                <div class="row">
+                                <div class="col-sm-12">
+                                <div class="mb-20">
+                                    <label for="status" class="form-label fw-semibold text-primary-light text-sm mb-8">Status <span class="text-danger-600">*</span> </label>
+                                    <select class="form-control radius-8 form-select" name="status" id="status">
+                                        <option value="1" @if($partnerData->status==1) selected @endif >Active</option>
+                                        <option value="0" @if($partnerData->status==0) selected @endif>Deactive</option>
+                                    </select>
+                                </div>
+                            </div>
+                                </div>
+                                
+                     
+                                <div>
+                                    <label class="form-label fw-bold text-neutral-900">Upload Logo </label>
+                                    @if(!empty($partnerData->logo))
+                                    <img src="{{url('/')}}/{{$partnerData->logo}}" style="width:120px;">
+                                    @endif
+                                    <input type="file" name="partner_logo[]" multiple class="form-control">
+                                </div>
+
+                                <button type="submit" class="btn btn-primary-600 radius-8">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+              
+            </div>            
+@endsection
