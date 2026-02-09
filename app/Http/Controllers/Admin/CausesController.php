716817866311a4110fa4cac0ca9d86e204eb462e
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-09 11:03:58 +0530

    Update CausesController.php

--- app/Http/Controllers/Admin/CausesController.php
+++ app/Http/Controllers/Admin/CausesController.php
@@ -8,32 +8,35 @@ use Illuminate\Support\Str;
 
 class CausesController extends Controller{
     
-public function causes_category(){
-    $allcat = CausesCategory::all();
-    return view('admin.causes.causes_category',compact('allcat'));
-}
-
-public function add_causes_category(Request $request){
-    if($request->isMethod('post')){
-        $request->validate([
-            'cat_name' => 'required|string|max:1000',
-            'status' => 'required|in:0,1',
-        ]);
-
-        CausesCategory::create([
-            'cat_name' => $request->cat_name,
-            'slug'     => Str::slug($request->cat_name),
-            'status'   => $request->status,
-        ]);
-
-        return redirect('admin/causes-category/')->with('success', 'Blog category added successfully!');
-
-    }else{
-        $allcat = array();
-        return view('admin.causes.add_causes_category',compact('allcat'));
+    public function causes_category(){
+        $allcat = CausesCategory::all();
+        return view('admin.causes.causes_category',compact('allcat'));
     }
-    
-}
+
+    public function add_causes_category(Request $request){
+        if($request->isMethod('post')){
+            $request->validate([
+                'cat_name' => 'required|string|max:1000',
+                'status' => 'required|in:0,1',
+            ]);
+
+            CausesCategory::create([
+                'cat_name' => $request->cat_name,
+                'slug'     => Str::slug($request->cat_name),
+                'status'   => $request->status,
+            ]);
+
+            return redirect('admin/causes-category/')->with('success', 'Blog category added successfully!');
+
+        }else{
+            $allcat = array();
+            return view('admin.causes.add_causes_category',compact('allcat'));
+        }
+    }
+
+    public function edit_causes_category($id){
+        echo $id;
+    }
     public function manage_causes(Request $request){
         $keyFeature = array();
         return view('admin.causes.manage_causes',compact('keyFeature'));
