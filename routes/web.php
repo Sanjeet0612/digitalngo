34f8ef55b901a94c1a903f876d833a70b6f2b23b
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-09 11:27:12 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -202,7 +202,7 @@ Route::prefix('admin')->group(function () {
         Route::match(['get', 'post'], '/add-causes-category', [CausesController::class, 'add_causes_category'])->name('admin.add_causes_category');
         Route::get('/edit-causes-category/{id}', [CausesController::class, 'edit_causes_category'])->name('admin.edit_causes_category');
         Route::put('/update-causes-category/{id}', [CausesController::class, 'update_causes_category'])->name('admin.update_causes_category');
-        
+        Route::get('/delete-causes-category/{id}', [CausesController::class, 'delete_causes_category'])->name('admin.delete_causes_category');
 
         Route::get('manage-causes', [CausesController::class, 'manage_causes'])->name('admin.manage_causes');
         Route::match(['get', 'post'], '/add-cause', [CausesController::class, 'add_cause'])->name('admin.add_cause');
