b0ac4878580e84771ea8683e4f96a8492b5dc737
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-11 15:48:37 +0530

    Update GuestDonation.php

--- app/Http/Controllers/Admin/GuestDonation.php
+++ app/Http/Controllers/Admin/GuestDonation.php
@@ -12,4 +12,22 @@ class GuestDonation extends Controller
         $guestDonation = Donation::all();
         return view('admin.donation.guest_donation',compact('guestDonation'));
     }
+
+    public function edit_guest_donation($id){
+        $donationDetail = Donation::where('id',$id)->first();
+        return view('admin.donation.edit_guest_donation',compact('donationDetail'));
+    }
+
+    public function update_guest_donation(Request $request,$id){
+        if($request->isMethod('put')){
+            
+            $donationData = Donation::findOrFail($id);
+            //print_r($donationData); die();
+            $updated = $donationData->update([
+                'is_paid' => $request->is_paid,
+                'status'  => $request->status,
+            ]);
+            return redirect('admin/guest-donation')->with('success', 'Donation & Cause updated successfully ✅');
+        }
+    }
 }
