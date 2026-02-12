80f270e0fa5b068f66a33b484a75f007815a27c4
Sanjeet Kumar <sanjeetsinghyadav0612@gmail.com>
2026-02-12 13:02:47 +0530

    Update sidebar.blade.php

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -3,10 +3,10 @@
         <iconify-icon icon="radix-icons:cross-2"></iconify-icon>
     </button>
     <div>
-        <a href="{{ route('index') }}" class="sidebar-logo">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="site logo" class="light-logo">
-            <img src="{{ asset('assets/images/logo-light.png') }}" alt="site logo" class="dark-logo">
-            <img src="{{ asset('assets/images/logo-icon.png') }}" alt="site logo" class="logo-icon">
+        <a href="{{url('/')}}/dashboard" class="sidebar-logo">
+            <img src="{{ asset('assets/images/logo_mahror.jpeg') }}" alt="site logo" class="light-logo">
+            <img src="{{ asset('assets/images/logo_mahror.png') }}" alt="site logo" class="dark-logo">
+            <img src="{{ asset('assets/images/logo_mahror.jpeg') }}" alt="site logo" class="logo-icon">
         </a>
     </div>
     <div class="sidebar-menu-area">
